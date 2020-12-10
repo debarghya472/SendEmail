1b4b55c5b7d24a29d6404e75e4723b029952c114
debarghya472 <debarghya_1901me19@iitp.ac.in>
2020-12-11 02:40:49 +0530

    Configured basic nodemailer

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+    res.send("Hello email sender!");
+});
+
+app.post('/send',(req,res)=>{
+    const emailbody = `
+    <p>This is a test mail</p>
+    <h3>Contact </h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+  console.log(req.body.name, req.body.message);
+    let transporter = nodemailer.createTransport({
+        host: 'stud.iitp.ac.in',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: 'debarghya_1901me19@iitp.ac.in', // generated ethereal user
+            pass: 'Deb@9831105701'  // generated ethereal password
+        },
+        tls:{
+          rejectUnauthorized:false
+        }
+      });
+    
+    let mailOptions = {
+        from: '"Debarghya Maity" <debarghya_1901me19@iitp.ac.in>', // sender address
+        to:  'debarghyamaity@gmail.com', // list of receivers
+        subject: 'Just For Check', // Subject line
+        text: 'Hello world', // plain text body
+        html:  emailbody// html body
+    };  
+
+console.log(emailbody);
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            res.send("Unsuccessful attempt")
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);   
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        res.send("email sent successfully");
+    });
+
+});
+
+app.listen(2000, ()=>{
+    console.log('Server started at port 2000....');
+});
